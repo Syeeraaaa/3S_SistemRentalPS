da09fbee8fb27826462b85c4a8db5f8a78303a77
Syeeraaaa <syeerasilvia@gmai.com>
2026-04-15 10:42:57 +0700

    Menambahkan deklarasi SqlConnection dan SqlCommand lalu membuat method Koneksi()

---
+++ SistemRentalPS/SistemRentalPS/Dashboardcs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemRentalPS
+{
+    public partial class Dashboardcs : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+             "Data Source=DESKTOP-A1J1BDF\\SYEERA; Initial Catalog=SistemRental_PS; Integrated Security=True"
+            );
+        }
+        public Dashboardcs()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void dtDashboard_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Laporan_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTransaksi_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
