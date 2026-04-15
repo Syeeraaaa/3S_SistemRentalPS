495311501e444225633569e771dfd9b106cd9888
Syeeraaaa <syeerasilvia@gmai.com>
2026-04-15 14:03:17 +0700

    Membuat method koneksi

---
+++ SistemRentalPS/SistemRentalPS/DataPS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SistemRentalPS
+{
+    public partial class DataPS : Form
+    {
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        string id_unit;
+
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+             "Data Source=DESKTOP-A1J1BDF\\SYEERA; Initial Catalog=SistemRental_PS; Integrated Security=True"
+            );
+        }
+
+        public DataPS()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
+
