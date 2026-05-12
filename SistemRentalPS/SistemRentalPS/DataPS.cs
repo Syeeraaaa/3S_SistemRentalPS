928a98a0216e91ead4b2463046933919a437fb62
Syeeraaaa <syeerasilvia@gmai.com>
2026-05-13 04:56:09 +0700

    menambah validasi input Nama Unit

--- SistemRentalPS/SistemRentalPS/DataPS.cs
+++ SistemRentalPS/SistemRentalPS/DataPS.cs
@@ -98,65 +98,74 @@ namespace SistemRentalPS
             }
 
         }
-       
 
+        // ------------------------
+        // TOMBOL TAMBAHA BUAT UNIT
+        // ------------------------
         private void btnTambah_Click_1(object sender, EventArgs e)
         {
             try
             {
-                Koneksi();
-                if (conn.State == System.Data.ConnectionState.Closed)
+                using (SqlConnection conn = Koneksi())
                 {
                     conn.Open();
-                }
-                if (txtNamaUnit.Text == "")
-                {
-                    MessageBox.Show("Nama Unit harus diisi");
-                    txtNamaUnit.Focus();
-                    return;
-                }
-                if (txtTipePS.Text == "")
-                {
-                    MessageBox.Show("Tipe PS harus diisi");
-                    txtTipePS.Focus();
-                    return;
-                }
-                if (txtHargaJam.Text == "" || txtHargaJam.Text.Any(char.IsLetter))
-                {
-                    MessageBox.Show("Harga/Jam harus diisi");
-                    txtHargaJam.Focus();
-                    return;
-                }
-                if (cmbStatus.Text == "")
-                {
-                    MessageBox.Show("Status harus diisi");
-                    cmbStatus.Focus();
-                    return;
-                }
 
-                string query = @"INSERT INTO UnitPS
+
+                    if (string.IsNullOrWhiteSpace(txtNamaUnit.Text))
+                    {
+                        MessageBox.Show("Nama Unit harus diisi");
+                        txtNamaUnit.Focus();
+                        return;
+                    }
+                    if (txtNamaUnit.Text == "")
+                    {
+                        MessageBox.Show("Nama Unit harus diisi");
+                        txtNamaUnit.Focus();
+                        return;
+                    }
+                    if (txtTipePS.Text == "")
+                    {
+                        MessageBox.Show("Tipe PS harus diisi");
+                        txtTipePS.Focus();
+                        return;
+                    }
+                    if (txtHargaJam.Text == "" || txtHargaJam.Text.Any(char.IsLetter))
+                    {
+                        MessageBox.Show("Harga/Jam harus diisi");
+                        txtHargaJam.Focus();
+                        return;
+                    }
+                    if (cmbStatus.Text == "")
+                    {
+                        MessageBox.Show("Status harus diisi");
+                        cmbStatus.Focus();
+                        return;
+                    }
+
+                    string query = @"INSERT INTO UnitPS
                           ( nama_unit,tipe_ps,harga_perjam,status)
                           VALUES
                           ( @nama_unit,@tipe_ps,@harga_perjam,@status)";
-                SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlCommand cmd = new SqlCommand(query, conn);
 
-                //cmd.Parameters.AddWithValue("@id_unit", cmbPilihUnit.Text);
-                cmd.Parameters.AddWithValue("@nama_unit", txtNamaUnit.Text);
-                cmd.Parameters.AddWithValue("@tipe_ps", txtTipePS.Text);
-                cmd.Parameters.AddWithValue("@harga_perjam", txtHargaJam.Text);
-                cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
+                    //cmd.Parameters.AddWithValue("@id_unit", cmbPilihUnit.Text);
+                    cmd.Parameters.AddWithValue("@nama_unit", txtNamaUnit.Text);
+                    cmd.Parameters.AddWithValue("@tipe_ps", txtTipePS.Text);
+                    cmd.Parameters.AddWithValue("@harga_perjam", txtHargaJam.Text);
+                    cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
 
-                int result = cmd.ExecuteNonQuery();
+                    int result = cmd.ExecuteNonQuery();
 
-                if (result > 0)
-                {
-                    MessageBox.Show("Data Unit PS berhasil ditambahkan");
-                    ClearForm();
-                    btnTambah.PerformClick();
-                }
-                else
-                {
-                    MessageBox.Show("Data gagal ditambahkan");
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Data Unit PS berhasil ditambahkan");
+                        ClearForm();
+                        btnTambah.PerformClick();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data gagal ditambahkan");
+                    }
                 }
             }
             catch (Exception ex)
