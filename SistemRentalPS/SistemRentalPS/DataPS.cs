a22aaeb3839702fd3c93a24304ad068c52fc8392
Syeeraaaa <syeerasilvia@gmai.com>
2026-05-13 05:23:52 +0700

    Menyimpan game dgn stored procedure

--- SistemRentalPS/SistemRentalPS/DataPS.cs
+++ SistemRentalPS/SistemRentalPS/DataPS.cs
@@ -299,53 +299,58 @@ namespace SistemRentalPS
             try
             {
                 using (SqlConnection conn = Koneksi())
-                { 
-                }
-                    Koneksi();
-                if (conn.State == System.Data.ConnectionState.Closed)
                 {
-                    conn.Open();
-                }
-                if (txtNamaGame.Text == "")
-                {
-                    MessageBox.Show("Nama Game harus diisi");
-                    txtNamaGame.Focus();
-                    return;
-                }
-                if (cmbPilihUnit.SelectedValue == null)
-                {
-                    MessageBox.Show("Pilih Unit terlebih dahulu!");
-                    cmbPilihUnit.Focus();
-                    return;
-                }
-                if (cmbGenre.Text == "")
-                {
-                    MessageBox.Show("Pilih Genre Game terlebih dahulu!!");
-                    cmbGenre.Focus();
-                    return;
-                }
 
-                string query = @"INSERT INTO Game
+                    
+                    if (conn.State == System.Data.ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    if (txtNamaGame.Text == "")
+                    {
+                        MessageBox.Show("Nama Game harus diisi");
+                        txtNamaGame.Focus();
+                        return;
+                    }
+                    if (cmbPilihUnit.SelectedValue == null)
+                    {
+                        MessageBox.Show("Pilih Unit terlebih dahulu!");
+                        cmbPilihUnit.Focus();
+                        return;
+                    }
+                    if (cmbGenre.Text == "")
+                    {
+                        MessageBox.Show("Pilih Genre Game terlebih dahulu!!");
+                        cmbGenre.Focus();
+                        return;
+                    }
+
+                    string query = @"INSERT INTO Game
                           (id_unit,nama_game,genre)
                           VALUES
                           (@id_unit,@nama_game,@genre)";
-                SqlCommand cmd = new SqlCommand(query, conn);
+                    using (SqlCommand cmd = new SqlCommand("sp_InsertGamePS", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Clear();
 
-                cmd.Parameters.AddWithValue("@id_unit", cmbPilihUnit.SelectedValue);
-                cmd.Parameters.AddWithValue("@nama_game", txtNamaGame.Text);
-                cmd.Parameters.AddWithValue("@genre", cmbGenre.Text);
+                        cmd.Parameters.AddWithValue("@id_unit", cmbPilihUnit.SelectedValue);
+                        cmd.Parameters.AddWithValue("@nama_game", txtNamaGame.Text);
+                        cmd.Parameters.AddWithValue("@genre", cmbGenre.Text);
 
-                int result = cmd.ExecuteNonQuery();
+                        int result = cmd.ExecuteNonQuery();
 
-                if (result > 0)
-                {
-                    MessageBox.Show("Data Game berhasil ditambahkan");
-                    ClearForm();
-
-                }
-                else
-                {
-                    MessageBox.Show("Data gagal ditambahkan");
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Data Game berhasil ditambahkan");
+                            ClearForm();
+                            LoadComboUnit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data gagal ditambahkan");
+                        }
+                    }
                 }
             }
             catch (Exception ex)
