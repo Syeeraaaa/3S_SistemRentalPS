8527e93bd090f3e23a1b336dcfd7080ec37ab43a
Syeeraaaa <syeerasilvia@gmai.com>
2026-06-23 22:40:59 +0700

    menambah class untuk model data laporan

---
+++ SistemRentalPS/SistemRentalPS/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemRentalPS
+{
+    internal class Class1
+    {
+        public string Nama { get; set;}
+        public string nama_unit { get; set; }
+        public string tipe_ps { get; set; }
+        public DateTime tanggal { get; set; }
+        public string jam_mulai { get; set; }
+        public string jam_selesai { get; set; }
+        public decimal total_bayar { get; set; }
+    }
+}
